ed47d59d3c720b2e43e139c3bfd781a071423561
mauloli <94847021+mauloli@users.noreply.github.com>
2022-03-18 09:41:26 +0700

    task 3 program kedua

--- task3.js
+++ task3.js
@@ -1,7 +1,7 @@
 const luasJajarGenjang = (alas, tinggi) => new Promise((resolve, reject) => {
     let luas
     setTimeout(() => {
-        if (typeof alas != 'number') {
+        if (typeof (alas, tinggi) != 'number') {
             reject(new Error("alas dan tunggi harus angka !"))
         } else {
             luas = alas * tinggi
@@ -15,14 +15,38 @@ const menampilkanLuas = (luas) => {
 }
 
 
-let alas = 10
-let tinggi = 10
-luasJajarGenjang(alas, tinggi)
-    .then((luas) => (menampilkanLuas(luas)))
+// let alas = 10
+// let tinggi = 10
+// luasJajarGenjang(alas, tinggi)
+//     .then((luas) => (menampilkanLuas(luas)))
 
+//---------------------------------------------------------------------------------------------
 
-
-const kelilingJajarGenjang = () => new Promise((resolve, reject) => {
-
+const kelilingJajarGenjang = (alas, tinggi) => new Promise((resolve, reject) => {
+    let keliling
+    setTimeout(() => {
+        if (typeof (alas, tinggi) !== 'number') {
+            reject(new Error("alas dan tunggi harus angka !"))
+        } else {
+            keliling = 2 * (alas + tinggi)
+            resolve(keliling)
+        }
+    }, 3000)
 
 })
+
+const tampilkanKeliling = (keliling) => new Promise((resolve, reject) => {
+    console.log(`keliling jajar genjang = ${keliling}`)
+    resolve(keliling)
+})
+
+const hitung = async () => {
+    try {
+        keliling = await kelilingJajarGenjang(10, 10)
+        await tampilkanKeliling(keliling)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+hitung()
